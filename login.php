104da0f88bd940926955e0293b9b86d9844874b3
Rohit <rjakhmola5@gmail.com>
2018-02-25 05:33:19 +0530

    styling changed

--- login.php
+++ login.php
@@ -3,34 +3,34 @@ session_start();
 include 'config.php';
 if(isset($_POST['username']))
 {
-	$row=mysqli_query($con,"select id from user where username='".$_POST['username']."'' && binary password='".$_POST['password']."");
+    $row=mysqli_query($con,"select id from user where username='".$_POST['username']."'' && binary password='".$_POST['password']."");
     $num= mysqli_num_rows($row);
     if(num==1){
-	$_SESSION['user']=$_POST['password'];
+    $_SESSION['user']=$_POST['password'];
 }
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Login</title>
 </head>
 <body>
     <div class="login_page">
-    	<div class="form">
-    		<form class="register_form hidden" id="register_form">
-    			<input type="text" name="login_username" placeholder="Username">
-    			<input type="text" name="login_password" placeholder="Password">
-    			<input type="text" name="login_email" placeholder="Email">
-    			<button>Create</button>
+        <div class="form">
+            <form class="register_form hidden" id="register_form">
+                <input type="text" name="login_username" placeholder="Username" class="form_control_custom">
+                <input type="text" name="login_password" placeholder="Password" class="form_control_custom">
+                <input type="text" name="login_email" placeholder="Email" class="form_control_custom">
+                <button>Create</button>
                  <p class="message">Already Registered?<a href="#"> Login</a>
                  </p>
-    		</form>
+            </form>
             <form class="login_form" id="login_form">
-                <input type="text" name="login_username" placeholder="Username">
-                <input type="password" name="login_password" placeholder="Password">
-                <button>login</button>
+                <input type="text" name="login_username" placeholder="Username" class="form_control_custom">
+                <input type="password" name="login_password" placeholder="Password" class="form_control_custom">
+                <button>LOGIN</button>
                 <p class="message">Not Registered?<a href="#" id="msg">Register</a></p>
             </form>
         </div>
